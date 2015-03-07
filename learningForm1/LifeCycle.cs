ab6477986dfaba5c170857fd44b078bfa41204f1
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-03-08 00:11:00 +0200

    Life Cycle form added

---
+++ learningForm1/LifeCycle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learningForm1
+{
+    public partial class LifeCycle : Form
+    {
+        public LifeCycle()
+        {
+            InitializeComponent();
+        }
+
+        private void LifeCycle_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Oranges");
+            listBox1.Items.Add("Grapes");
+            listBox1.Items.Add("Bananas");
+            listBox1.Items.Add("Peaches");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = listBox1.Text;
+        }
+    }
+}
--- learningForm1/Program.cs
+++ learningForm1/Program.cs
@@ -17,7 +17,8 @@ namespace learningForm1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MyForm());
+            //Application.Run(new MyForm());
+            Application.Run(new LifeCycle());
         }
     }
 }
